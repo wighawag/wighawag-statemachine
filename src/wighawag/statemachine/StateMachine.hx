078ff18716ecb097a29d427b7362ddb019daf36a
Wighawag <wighawag@gmail.com>
2013-07-17 13:42:22 +0530

    fixes

--- src/wighawag/statemachine/StateMachine.hx
+++ src/wighawag/statemachine/StateMachine.hx
@@ -14,7 +14,7 @@ class StateMachine<T> {
 
     private var transitions : Map<State<T>, Map<String,State<T>>>;
 
-    private var states : Map<Class<State<T>>, State<T>>;
+    //private var states : Map<Class<State<T>>, State<T>>;
 
     private var model : T;
 
@@ -24,7 +24,10 @@ class StateMachine<T> {
 
     private var alreadyRun : Bool;
 
+    private var states : Map<State<T>, Bool>;
+
     public function new(model : T) {
+        states = new Map();
         this.model = model;
         eventer = new Signal1<String>();
         eventer.add(onEvent);
@@ -60,17 +63,45 @@ class StateMachine<T> {
             hashSet = new Map<String,State<T>>();
         }
         hashSet.set(event,nextState);
+        Report.anInfo("StateMachine", "add Transition ", prevState, event, nextState);
         transitions.set(prevState,hashSet);
-    }
+        states.set(prevState, true);
 
-    public function setStartState(startingState : Class<State<T>>){
-        var state = states.get(startingState);
-        if (state == null){
-            throw "the starting state need to exist first";
-
+        Report.anInfo("StateMachine", "-------------------");
+        for(state in states.keys()){
+            Report.anInfo("StateMachine", "            ------------");
+            if(state == prevState){
+                Report.anInfo("StateMachine", "-", state, "same as ", prevState);
+                if(transitions.get(prevState) == null){
+                    Report.anError("StateMachine", "-", "same but not saved", prevState);
+                }else{
+                    Report.anInfo("StateMachine", "-", "same and saved", prevState);
+                }
+            }
+            if(transitions.get(state) == null){
+                Report.anError("StateMachine", "transition not saved", state);
+            }else{
+                Report.anInfo("StateMachine", "transition saved", state);
+            }
+            if(transitions.get(prevState) == null){
+                Report.anError("StateMachine", " same transition not saved", prevState);
+            }else{
+                Report.anInfo("StateMachine", " same transition saved", prevState);
+            }
+            Report.anInfo("StateMachine", "            ------- end -----");
         }
-        currentState = state;
+        Report.anInfo("StateMachine", "----------- end --------");
+
     }
+
+//    public function setStartState(startingState : Class<State<T>>){
+//        var state = states.get(startingState);
+//        if (state == null){
+//            throw "the starting state need to exist first";
+//
+//        }
+//        currentState = state;
+//    }
 
     /**
     *   Update the state machine and its current state
@@ -87,9 +118,13 @@ class StateMachine<T> {
 
     private function onEvent(value : String) : Void{
         var eventHash = transitions.get(currentState);
+        if(eventHash == null){
+            Report.anError("StateMachine", "no transition for state : ", currentState);
+            return;
+        }
         var nextState = eventHash.get(value);
         if (nextState == null){
-            Report.anError("StateMachine", "no state for ", currentState, value);
+            Report.anError("StateMachine", "no state transition for the duo: ", currentState, value);
             return;
         }
         currentState.onLeave(model);
